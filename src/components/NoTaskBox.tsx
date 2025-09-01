f44d29cb7978617b5e97cc17481f7e5584920fd6
ckdgus1010 <ckdgus3735@gmail.com>
2025-09-01 12:22:45 +0900

    styles: 홈 화면 제목을 가운데 정렬하도록 수정

--- src/components/NoTaskBox.tsx
+++ src/components/NoTaskBox.tsx
@@ -14,6 +14,7 @@ const Title = styled.div`
     margin-bottom: 32px;
     font-weight: 500;
     font-size: 2rem;
+    text-align: center;
     color: ${props => props.theme.text.title};
     transition: color 0.2s ease;
 `;
